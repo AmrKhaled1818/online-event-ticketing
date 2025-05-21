f960c4588888fd01c76281bb8c3a90137aeca083
Eyad Hussein <eyadhussein@Eyads-MacBook-Air.local>
2025-05-21 22:29:59 +0300

    events automatically approved

--- backend/models/Events.js
+++ backend/models/Events.js
@@ -13,7 +13,7 @@ const EventSchema = new mongoose.Schema({
     status: {
         type: String,
         enum: ["pending", "approved", "declined"],
-        default: "pending"
+        default: "approved"
       },
 }, { timestamps: true });
 
