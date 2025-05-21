af044ab396f41e64ba7483933470cb6814074df2
Eyad Hussein <eyadhussein@Eyads-MacBook-Air.local>
2025-05-21 22:44:46 +0300

    fixed padding between event lists

--- backend/models/Events.js
+++ backend/models/Events.js
@@ -13,7 +13,7 @@ const EventSchema = new mongoose.Schema({
     status: {
         type: String,
         enum: ["pending", "approved", "declined"],
-        default: "approved"
+        default: "pending"
       },
 }, { timestamps: true });
 
